cb0d51c078ae77890f3a8cbea285158c8c3c3d96
rovala <rovalasiul@gmail.com>
2023-06-14 23:32:09 -0500

    Homework 04

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,7 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   return (x>y?x:y)
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +13,7 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   return (edad>=18?'Allowed':'Not allowed')
 }
 
 function conection(status) {
@@ -21,6 +23,7 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   return (status===1?'Online':(status===2?'Away':'Offline'))
 }
 
 function saludo(idioma) {
@@ -30,6 +33,7 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   return (idioma==='aleman'?'Guten Tag!':(idioma==='mandarin'?'Ni Hao!':(idioma==='ingles'?'Hello!':'Hola!')))
 }
 
 function colors(color) {
@@ -41,18 +45,21 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   return (color==='blue'?'This is blue':(color==='red'?'This is red':(color==='green'?'This is green':(color==='orange'?'This is orange':'Color not found'))))
 }
 
 function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   return (num===10||num===5)?true:false
 }
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
+   return (num>20 && num<50)?true:false
 }
 
 function esEntero(num) {
@@ -62,6 +69,7 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   return (Number.isInteger(num)?true:false)
 }
 
 function fizzBuzz(num) {
@@ -70,6 +78,10 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   if (num%3===0&&num%5===0) return 'fizzbuzz';
+   if (num%3===0) return 'fizz';
+   if (num%5===0) return 'buzz';
+   return false;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -80,6 +92,7 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   return (num1>num2&&num1>num3&&num1>0)?'Numero 1 es mayor y positivo':((num1<0||num2<0||num3<0)?'Hay negativos':(num3>num1&&num3>num2?num3+1:(num1===0&&num2===0&&num3===0?'Error':false) ))
 }
 
 function esPrimo(num) {
@@ -89,18 +102,37 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   if (num<=1){
+      return false
+   }
+   else{
+      for (i=2;i*i<=num;i++){
+         if (num%i===0){
+            return false
+         }
+      }
+      return true
+   }
+
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   return (valor===true?'Soy verdadero':'Soy falso')
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if (num>=100&&num<1000){
+      return true
+   }
+   else{
+      return false
+   }
 }
 
 function doWhile(num) {
@@ -108,6 +140,13 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   var limite=1;
+   do{
+      num=num+5
+      limite++;
+   }
+   while (limite<=8)
+   return (num)
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
